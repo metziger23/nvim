59e3cdbc67db3d6f2d395489ad83d1c1b6a80201
metziger23 <metziger23@gmail.com>
2024-02-02 21:50:59 +0300

    put plugins into plugins dir

--- init.lua
+++ init.lua
@@ -35,48 +35,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
-local plugins = {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }, 
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		}
-	}
-}
-local opts = {}
+require("lazy").setup("plugins")
 
 
-require("lazy").setup(plugins, opts)
 
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-
-vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {})
-vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-	ensure_installed = {"lua", "c", "cpp", "bash"},
-	highlight = { enable = true },
-	indent = { enable = true },  
-})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
