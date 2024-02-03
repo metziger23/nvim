c98f8c0fb8506e66aa9fac79996d09fa0175911c
metziger23 <metziger23@gmail.com>
2024-02-03 13:01:28 +0300

    added lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "catppuccin",
+        globalstatus = true,
+      },
+    })
+  end,
+}
