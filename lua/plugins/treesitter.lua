2a6d40849c7018a31cff99ab1d4dc9af53decb2a
metziger23 <metziger23@gmail.com>
2024-02-04 22:06:19 +0300

    changes in telescope

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,14 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = {"lua", "c", "cpp", "bash", "vimdoc"},
-        highlight = { enable = true },
-        indent = { enable = true },  
-      })
-
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = { "lua", "c", "cpp", "bash", "vim" },
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
