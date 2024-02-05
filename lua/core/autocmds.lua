7a34cd0d4856c8b25e51c99691e55bbbf101a8ba
metziger23 <metziger23@gmail.com>
2024-02-05 21:54:46 +0300

    added highlight on yank

---
+++ lua/core/autocmds.lua
@@ -0,0 +1,17 @@
+-- [[ Highlight on yank ]]
+local setup_highlight_on_yank = function()
+  -- See `:help vim.highlight.on_yank()`
+  local highlight_group = vim.api.nvim_create_augroup(
+    "YankHighlight",
+    { clear = true }
+  )
+  vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+      vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = "*",
+  })
+end
+
+setup_highlight_on_yank()
