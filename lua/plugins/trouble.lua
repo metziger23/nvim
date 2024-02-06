626c31fe9b60699e2b9f35651ea67f303573087b
metziger23 <metziger23@gmail.com>
2024-02-06 19:52:50 +0300

    Added some keymaps to trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,10 +1,9 @@
 return {
 	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		use_diagnostic_signs = true,
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+	cmd = { "TroubleToggle", "Trouble" },
+	opts = { use_diagnostic_signs = true },
+	keys = {
+		{ "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble" },
+		{ "<leader>xR", "<cmd>TroubleToggle lsp_references<cr>", desc = "Toggle Trouble LSP References" },
 	},
 }
