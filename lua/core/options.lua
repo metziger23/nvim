fadce973885d343190e491360ebce6e333b4e359
metziger23 <metziger23@gmail.com>
2024-02-11 15:13:06 +0300

    Bordered window for cmp + changes options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,14 +8,27 @@ opt.number = true -- shows absolute line number on cursor line (when relative nu
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
+opt.smartindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
 
+-- Better splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Enable mouse mode
+vim.opt.mouse = "a"
+
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = { "menuone", "noselect" }
+
+-- Enable 24-bit color
+vim.opt.termguicolors = true
 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
@@ -24,3 +37,7 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- turn off swapfile
 opt.swapfile = false
+
+
+-- Always keep 8 lines above/below cursor unless at start/end of file
+vim.opt.scrolloff = 8
