72e8258ec4df1080f325928ce03f78b3a6a81b47
metziger23 <metziger23@gmail.com>
2024-02-25 13:00:04 +0300

    returned to default catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,13 +6,6 @@ return {
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
-				color_overrides = {
-					mocha = {
-						base = "#000000",
-						mantle = "#000000",
-						crust = "#000000",
-					},
-				},
 				floating_border = "on",
 			})
 			vim.cmd.colorscheme("catppuccin")
