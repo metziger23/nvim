bf77af73879b0d8149328bd96768ccd2a14d7e67
metziger23 <metziger23@gmail.com>
2024-03-03 12:15:07 +0300

    round file popup for nvimtree + changed some dashboard buttons

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -36,6 +36,11 @@ return {
       -- explorer to work well with
       -- window splits
       actions = {
+        file_popup = {
+          open_win_config = {
+            border = "rounded",
+          },
+        },
         open_file = {
           window_picker = {
             enable = false,
@@ -53,9 +58,6 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
   end,
 }
