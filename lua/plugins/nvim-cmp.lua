6d0363b5d86d51609729d894093922e62101f0ca
metziger23 <metziger23@gmail.com>
2024-03-17 14:49:49 +0300

    added tabs to luasnip

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -23,6 +23,11 @@ return {
 		"hrsh7th/cmp-cmdline",
 	},
 	config = function()
+
+    local check_backspace = function()
+      local col = vim.fn.col "." - 1
+      return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+    end
 		local cmp = require("cmp")
 
 		local luasnip = require("luasnip")
@@ -76,6 +81,34 @@ return {
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif check_backspace() then
+            fallback()
+          else
+            fallback()
+          end
+        end, {
+            "i",
+            "s",
+          }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {
+            "i",
+            "s",
+          }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
