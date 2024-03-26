44b975bf1547735b93cef01a3a97c5f0ea951968
metziger23 <metziger23@gmail.com>
2024-03-26 23:26:07 +0300

    added new telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -33,13 +33,27 @@ return {
 		telescope.load_extension("file_browser")
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help Tags" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
-		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find Old Files" })
-		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find Word Under Cursor" })
-		vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Find Marks" })
+    local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+		keymap.set("n", "<leader>fF", builtin.current_buffer_fuzzy_find, { desc = "Current Buffer Fuzzy Find" })
+		keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+		keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help Tags" })
+		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+		keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find Old Files" })
+		keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find Word Under Cursor" })
+		keymap.set("n", "<leader>fm", builtin.marks, { desc = "Find Marks" })
+		keymap.set("n", "<leader>fM", builtin.man_pages, { desc = "Find Man Pages" })
+		keymap.set("n", "<leader>fc", builtin.command_history, { desc = "Command History" })
+		keymap.set("n", "<leader>fC", builtin.commands, { desc = "Commands" })
+		keymap.set("n", "<leader>fs", builtin.search_history, { desc = "Search History" })
+		keymap.set("n", "<leader>fq", builtin.quickfixhistory, { desc = "Quickfix History" })
+		keymap.set("n", "<leader>fQ", builtin.quickfix, { desc = "Quickfix" })
+		keymap.set("n", "<leader>fl", builtin.loclist, { desc = "Location List" })
+		keymap.set("n", "<leader>fj", builtin.jumplist, { desc = "Jump List" })
+		keymap.set("n", "<leader>fr", builtin.registers, { desc = "Registers" })
+		keymap.set("n", "<leader>fp", builtin.resume, { desc = "Previous Picker's Results" })
+		keymap.set("n", "<leader>fP", builtin.pickers, { desc = "List Previous Pickers" })
 
 		local opts = { noremap = true }
 		opts.desc = "Open Explorer"
